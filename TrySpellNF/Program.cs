ef44bd01094994f31987e9b53150c120acecfe2b
everonzuk <anyoneamit@gmail.com>
2020-06-03 19:37:56 +0530

    Setup
    SetUp

---
+++ TrySpellNF/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrySpellNF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string isNegative = "";
+            try
+            {
+                Console.WriteLine("Enter a Number to convert to currency");
+                string number = Console.ReadLine();
+                number = Convert.ToDouble(number).ToString();
+
+                if (number.Contains("-"))
+                {
+                    isNegative = "Minus ";
+                    number = number.Substring(1, number.Length - 1);
+                }
+                if (number == "0")
+                {
+                    Console.WriteLine("The number in currency fomat is \nZero Only");
+                }
+                else
+                {
+                    Console.WriteLine("The number in currency fomat is \n{0}", isNegative + LogictoSpell.ConvertToWords(number));
+                }
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrySpellNF;
+
+namespace UnitTestProjectTrySpell
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Failed", "Failed");
+        }
+
+       
+        [TestMethod]
+        public void CheckSpellInvalid()
+        {
+            string myword = LogictoSpell.ConvertToWords("54234.871");
+            string myActual = "Fifty Four Thousand Two Hundred Thirty Four Rupees and Eighty Seven Paisa Only";
+            Assert.AreNotEqual(myword, myActual);
+
+        }
+
+        /// <summary>
+        /// Test to check correct spell
+        /// </summary>
+        [TestMethod]
+        public void CheckSpellValid()
+        {
+            string myword = LogictoSpell.ConvertToWords("54234.87");
+            string myActual = "Fifty Four Thousand Two Hundred Thirty Four Rupees and Eighty Seven Paisa Only";
+            Assert.AreEqual(myword, myActual);
+
+        }
+
+        [TestMethod]
+        public void CheckSpellNegative()
+        {
+            string myword = LogictoSpell.ConvertToWords("-54234.87");
+            string myActual = "Value is negative";
+            Assert.AreEqual(myword, myActual);
+
+        }
+    }
+}
